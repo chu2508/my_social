0451df697c39ef83e4007decc5fc7e7e9d143d65
dirac <767848898@qq.com>
2021-08-27 19:08:02 +0800

    add bottom operate button

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,24 +4,44 @@ import { usePageScroll } from "@tarojs/taro";
 import classNames from "classnames";
 import React, { useRef, useState } from "react";
 import styles from "./style.module.scss";
+import { BsFillHeartFill } from "react-icons/bs";
+import { BiX } from "react-icons/bi";
 
 const Home = () => {
   const [navOut, setNavOut] = useState(false);
-  const ref = useRef(0)
+  const ref = useRef(0);
   usePageScroll(payload => {
-    const top = ref.current
+    const top = ref.current;
     const isOut = payload.scrollTop - top > 0;
     setNavOut(isOut);
-    ref.current = payload.scrollTop
+    ref.current = payload.scrollTop;
   });
   return (
     <View>
       <View style={{ height: "200vh", background: "green" }}></View>
-      <BottomNavBar
+      <View
         className={classNames(styles.bottom_nav, [
           navOut ? styles.isOut : styles.isIn
         ])}
-      />
+      >
+        <View
+          style={{ display: "flex", justifyContent: "center", color: "#fff", position: 'absolute', top: '-100%', left: 0, right: 0, zIndex: 199 }}
+        >
+          <View className={styles.operate_btn} style={{ background: "#999", marginRight: '20px' }}>
+            <BiX size={38} />
+          </View>
+          <View className={styles.operate_btn} style={{ background: "tomato" }}>
+            <BsFillHeartFill
+              size={24}
+              style={{ top: 2, position: "relative" }}
+            />
+          </View>
+        </View>
+        <BottomNavBar
+          className={styles.bottom_nav_shadow}
+          style={{ position: "relative" }}
+        />
+      </View>
     </View>
   );
 };
