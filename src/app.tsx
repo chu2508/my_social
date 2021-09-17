042f223578672b2616f0c39ee1408c468d7c9e52
zheng.liuli <767848898@qq.com>
2021-09-17 18:38:18 +0800

    add authentication service

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,17 @@
+import useAuthenticationService, { AuthenticationService } from "@bis/Authentication/useAuthenticationService";
 import { FC } from "@tarojs/taro";
 import { Provider } from "react-redux";
-import "./app.scss";
 import useHomeService, { HomeService } from "./pages/home/hooks/useHomeService";
+import "./app.scss";
 import configStore from "./store";
 
 const store = configStore();
 const App: FC = (props) => {
   return (
     <Provider store={store}>
-      <HomeService.Provider value={useHomeService()}>{props.children}</HomeService.Provider>
+      <AuthenticationService.Provider value={useAuthenticationService()}>
+        <HomeService.Provider value={useHomeService()}>{props.children}</HomeService.Provider>
+      </AuthenticationService.Provider>
     </Provider>
   );
 }
---
+++ src/components/business/Authentication/useAuthenticationService.ts
@@ -0,0 +1,39 @@
+import { getCurrentUser } from "@src/services/auth/index";
+import getServiceToken from "@src/tools/getServiceToken";
+import IUserProfile from "@src/types/IUserProfile";
+import { useEffect, useState } from "react";
+
+const touristProfile = {
+  id: "0",
+  isTourist: true,
+  nickname: "Tourist"
+};
+
+/**
+ * 用户认证服务，返回用户的账户相关信息，以及登录注册方法
+ * @returns 
+ */
+export default function useAuthenticationService() {
+  const [profile, setProfile] = useState<IUserProfile>(touristProfile);
+
+  const signUp = async (phone: number) => {};
+  const signIn = async () => {
+    setProfile({ id: "1", nickname: "Dirac", isTourist: false });
+  };
+  const signOut = async () => {
+    setProfile({ ...touristProfile });
+  };
+
+  useEffect(() => {
+    getCurrentUser().then(setProfile);
+  }, []);
+
+  return {
+    profile,
+    signUp,
+    signIn,
+    signOut
+  };
+}
+
+export const AuthenticationService = getServiceToken(useAuthenticationService);
