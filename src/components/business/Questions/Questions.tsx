7de35c762b06f69da09fb0efbce611ecc4ad0722
dirac <767848898@qq.com>
2021-09-20 18:53:46 +0800

    change question

--- src/components/business/Questions/Questions.tsx
+++ src/components/business/Questions/Questions.tsx
@@ -1,50 +1,91 @@
 import useToggle from "@src/tools/useToggle";
 import { View } from "@tarojs/components";
+import { ViewProps } from "@tarojs/components/types/View";
+import { useDidHide } from "@tarojs/taro";
+import Panel from "@ui/Panel";
+import classNames from "classnames";
+import { useContext } from "react";
 import { BiChevronRight } from "react-icons/bi";
-import { useQuestions } from "./useQuestions";
+import styles from "./style.module.scss";
+import useQuestionsService, {
+  Question,
+  QuestionsService
+} from "./useQuestions";
 
-const Questions = () => {
-  const { questions, confirmQuestion, firstQuestion } = useQuestions();
-  const { flag, toggle } = useToggle();
-  return firstQuestion ? (
-    <View>
-      <View>
-        <View>
-          做任务赚取青藤币
-          <View onClick={toggle}>
-            更多 <BiChevronRight />
-          </View>
-        </View>
-        <View>
-          <View>
-            <View>{firstQuestion.name} </View>
-            <View>{firstQuestion.reward}</View>
-          </View>
-          <View onClick={firstQuestion.execAction.bind(firstQuestion)}>
-            去完成
-          </View>
-        </View>
+const QuestionItem = (props: { question: Question }) => {
+  const { receiveReward } = useContext(QuestionsService);
+  const { question } = props;
+  const btnClass = classNames(styles.item_btn, {
+    [styles.active]: question.isResolved,
+    [styles.confirm]: question.isReceived
+  });
+  return (
+    <View className={styles.item}>
+      <View className={styles.item_left}>
+        <View className={styles.title}>{question.name} </View>
+        <View className={styles.sub_title}>{question.reward}</View>
       </View>
-      <View style={{ display: flag ? "block" : "none" }}>
-        {questions.map((question, key) => {
-          return (
-            <View key={key}>
-              <View>
-                <View>{question.name} </View>
-                <View>{question.reward}</View>
-              </View>
-              {question.isPending && (
-                <View onClick={question.execAction.bind(question)}>去完成</View>
-              )}
-              {question.isResolve && (
-                <View onClick={() => confirmQuestion(question)}>完成</View>
-              )}
-            </View>
-          );
-        })}
+      {question.isReceived && <View className={btnClass}>已完成</View>}
+      {question.isPending && (
+        <View className={btnClass} onClick={question.execAction}>
+          去完成
+        </View>
+      )}
+      {question.isResolved && (
+        <View className={btnClass} onClick={() => receiveReward(question)}>
+          领取奖励
+        </View>
+      )}
+    </View>
+  );
+};
+
+const Dialog = (props: { onMaskClick?: () => void }) => {
+  const { questions } = useContext(QuestionsService);
+
+  return (
+    <View className={styles.dialog}>
+      <View className={styles.mask} onClick={props.onMaskClick} />
+      <View className={styles.container}>
+        {questions.map((q, key) => (
+          <QuestionItem key={key} question={q} />
+        ))}
       </View>
     </View>
-  ) : null;
+  );
+};
+
+const QuestionPanelHead = (props: ViewProps) => {
+  return (
+    <View
+      {...props}
+      className={classNames("my-panel__header mb-30", styles.panel_head)}
+    >
+      <View className="my-panel__title">做任务得青藤币</View>
+      <View className={styles.head_right}>
+        查看更多任务
+        <BiChevronRight size={20} />
+      </View>
+    </View>
+  );
+};
+
+const Questions = () => {
+  const state = useQuestionsService();
+  const { flag, toggle, left } = useToggle();
+  useDidHide(() => {
+    left()
+  })
+  return (
+    <QuestionsService.Provider value={state}>
+      {state.firstQuestion && (
+        <Panel renderHeader={() => <QuestionPanelHead onClick={toggle} />}>
+          <QuestionItem question={state.firstQuestion} />
+        </Panel>
+      )}
+      {flag && <Dialog onMaskClick={toggle} />}
+    </QuestionsService.Provider>
+  );
 };
 
 export default Questions;
--- src/components/business/Questions/useQuestions.ts
+++ src/components/business/Questions/useQuestions.ts
@@ -1,39 +1,51 @@
-import Taro, { useDidShow } from "@tarojs/taro";
+import getServiceToken from "@src/tools/getServiceToken";
+import Taro from "@tarojs/taro";
 import { useState } from "react";
 
 enum QuestionStatus {
   PENDING,
   RESOLVED,
-  CONFIRMED
+  RECEIVED
 }
+
+export class Question {
+  id: string;
+  name: string = "Q1";
+  reward: string = "";
+  status: QuestionStatus = QuestionStatus.PENDING;
+  readonly action = {
+    type: "navigate",
+    url: "/pages/home/index"
+  };
+  get isResolved() {
+    return this.status === QuestionStatus.RESOLVED;
+  }
+  get isPending() {
+    return this.status === QuestionStatus.PENDING;
+  }
+  get isReceived() {
+    return this.status === QuestionStatus.RECEIVED;
+  }
+  receive = () => {
+    if (this.status === QuestionStatus.RESOLVED) {
+      this.status = QuestionStatus.RECEIVED;
+    }
+  };
+  execAction = () => {
+    if (this.action.type === "navigate") {
+      Taro.navigateTo({ url: this.action.url });
+    }
+  };
+}
+
 const createMockData = () => {
   const mockData = Array.from({ length: 10 }, (_, key) => {
-    return {
-      id: String(key + 1),
-      name: "任务" + key,
-      reward: "+60青藤币",
-      status: QuestionStatus.PENDING,
-      action: {
-        type: "navigate",
-        url: "/pages/home/index"
-      },
-      get isResolve() {
-        return this.status === QuestionStatus.RESOLVED;
-      },
-      get isPending() {
-        return this.status === QuestionStatus.PENDING;
-      },
-      confirm(this: { status: QuestionStatus }) {
-        if (this.status === QuestionStatus.RESOLVED) {
-          this.status = QuestionStatus.CONFIRMED;
-        }
-      },
-      execAction() {
-        if (this.action.type === "navigate") {
-          Taro.navigateTo({ url: this.action.url });
-        }
-      }
-    };
+    const q = new Question();
+    q.id = String(key + 1);
+    q.name = "任务" + key;
+    q.reward = "+60青藤币";
+    q.status = key % 2 === 0 ? QuestionStatus.RESOLVED : QuestionStatus.PENDING;
+    return q;
   });
   return mockData;
 };
@@ -42,22 +54,21 @@ const createMockData = () => {
  * 任务服务，每次onShow函数触发，都会获取最新的任务状态
  * @returns
  */
-export function useQuestions() {
+export default function useQuestionsService() {
   const [questions, setQuestions] = useState(createMockData());
-  const firstQuestion = questions.find(question => question.isPending);
-  const confirmQuestion = (question: any) => {
+  const firstQuestion = questions
+    .find(question => !question.isReceived);
+  const receiveReward = (question: Question) => {
     setQuestions(state => {
       if (question) {
-        question.confirm();
+        question.receive();
         return [...state];
       }
       return state;
     });
   };
 
-  useDidShow(() => {
-    setQuestions(createMockData());
-  });
-
-  return { questions, confirmQuestion, firstQuestion };
+  return { questions, receiveReward, firstQuestion };
 }
+
+export const QuestionsService = getServiceToken(useQuestionsService);
